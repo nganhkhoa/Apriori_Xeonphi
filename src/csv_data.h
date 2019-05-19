a4c80a727df3d96c5810b413bf30101fe14ea29d
Bao. Nguyen Le Chi <baonlc@vng.com.vn>
2019-05-19 13:34:54 +0700

    Update something

--- src/csv_data.h
+++ src/csv_data.h
@@ -1,8 +1,17 @@
-struct csv_data_t {
-  int Pclass;
-  int Sex;
-  int Age;
-  int Survived;
+#include <vector>
+#include <set>
+#include <functional>
+#include <string>
+#include <map>
+
+typedef std::vector<std::string> record_t;
+
+struct setFrequency_t {
+  // std::set<std::string> itemSet;
+  std::map<std::string, std::string> itemSet;
+  int support = 0;
 };
 
-void read_csv(char* filename, void* returned_data, int* total_data_count);
+void read_csv(
+    char* filename, std::vector<record_t>* records,
+    std::vector<char*> selected_fields);
